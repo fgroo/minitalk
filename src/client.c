281f8577af1f077af72852dcc09ea9a8d56bcea1
fgroo <faris.goerlich@gmail.com>
2025-06-15 01:34:11 +0200

    rework, dynmem string, no use of pause makes the code robust.

--- src/client.c
+++ src/client.c
@@ -1,16 +1,26 @@
 #include "minitalk.h"
 
+volatile sig_atomic_t	g_ack_received = 1;
+
 static void	ft_write(int fd, const char *str, size_t len)
 {
 	if (write(fd, str, len) < 0)
 		exit(0);
 }
 
+void	send_signal_and_wait(int pid, int signum)
+{
+	while (g_ack_received == 0)
+		;
+	g_ack_received = 0;
+	kill(pid, signum);
+}
+
 static void	ack_handler(int signum)
 {
+	g_ack_received = 1;
 	if (signum != SIGUSR2)
 		exit (0);
-	ft_write(1, "Client: OK!\n", 12);
 }
 
 int	send_char(int server_pid, char c)
@@ -20,6 +30,9 @@ int	send_char(int server_pid, char c)
 	i = -1;
 	while (++i < 8)
 	{
+		while (g_ack_received == 0)
+			;
+		g_ack_received = 0;
 		if ((c >> i) & 1)
 		{
 			if (kill(server_pid, SIGUSR1) == -1)
@@ -30,7 +43,6 @@ int	send_char(int server_pid, char c)
 			if (kill(server_pid, SIGUSR2) == -1)
 				return (0);
 		}
-		pause();
 	}
 	return (1);
 }
@@ -42,19 +54,21 @@ int	main(int argc, char **argv)
 	char				*message;
 	int					i;
 
-	i = 0;
 	if (argc != 3)
 		return (ft_printf("Usage: %s <Server_PID> <Message>\n", argv[0]), 1);
 	server_pid = ft_atoi(argv[1]);
 	message = argv[2];
 	if (server_pid <= 0)
 		return (ft_printf("Invalid PID.\n"), 1);
-	ft_printf("Sending a signal to Server PID: %d\n", server_pid);
 	ft_printf("Client PID: %d\n", getpid());
 	sa_client.sa_handler = ack_handler;
 	sigemptyset(&sa_client.sa_mask);
 	if (sigaction(SIGUSR2, &sa_client, NULL) == -1)
 		return (ft_write(2, "CLIENT: Error setting up SIGUSR2", 32), 1);
+	i = (ft_strlen(message) + 1) * 8;
+	while (i && i--)
+		send_signal_and_wait(server_pid, SIGUSR1);
+	send_signal_and_wait(server_pid, SIGUSR2);
 	while (message[i] && send_char(server_pid, message[i]))
 		i++;
 	send_char(server_pid, 0);
--- src/server.c
+++ src/server.c
@@ -1,37 +1,87 @@
 #include "minitalk.h"
 
+volatile int	g_len = 0;
+
 static void	ft_write_u(int fd, const unsigned char *str, size_t len)
 {
+	if (!str)
+		return ;
 	if (write(fd, str, len) < 0)
 	{
 	}
 }
 
-void	server_handler(int signum, siginfo_t *info, void *context)
+void	insert_bits(int signum, int *max_len, unsigned char	**buffer_string)
 {
 	static int				bit_index = 0;
 	static unsigned char	c = 0;
 
-	(void)context;
 	if (signum == SIGINT)
 		exit(0);
+	if (!*max_len && signum == SIGUSR1 && ++bit_index)
+		return ;
+	if (!*max_len && signum == SIGUSR2)
+	{
+		*max_len = bit_index / 8;
+		bit_index = 0;
+		*buffer_string = malloc(*max_len);
+		if (!buffer_string)
+			exit(1);
+		return ;
+	}
 	if (signum == SIGUSR1)
 		c |= (1 << bit_index);
 	bit_index++;
 	if (bit_index == 8)
 	{
-		if (c == 0)
-		{
-			ft_write_u(1, (unsigned char *)"\nString Complete. PID: ", 23);
-			ft_printf("%d\n", info->si_pid);
-		}
-		else
-			ft_write_u(1, &c, 1);
+		(*buffer_string)[g_len++] = c;
 		bit_index = 0;
 		c = 0;
 	}
-	usleep(1);
+}
+
+void	server_handler(int signum, siginfo_t *info, void *context)
+{
+	static unsigned char	*buffer_string;
+	static int				max_len = 0;
+
+	(void)context;
+	insert_bits(signum, &max_len, &buffer_string);
 	kill(info->si_pid, SIGUSR2);
+	if (max_len && g_len > max_len)
+	{
+		ft_write_u(2, (unsigned char *)"Error while transmitting", 24);
+		exit(1);
+	}
+	if (max_len && g_len == max_len)
+	{
+		ft_write_u(1, buffer_string, max_len);
+		ft_write_u(1, (unsigned char *)"\n", 1);
+		free(buffer_string);
+		g_len = 0;
+		max_len = 0;
+		return ;
+	}
+}
+
+void	for_sigaction(struct sigaction *sa)
+{
+	int	error_flag;
+
+	error_flag = 0;
+	sa->sa_flags = SA_SIGINFO;
+	sa->sa_handler = NULL;
+	sa->sa_sigaction = server_handler;
+	sigemptyset(&sa->sa_mask);
+	if (sigaction(SIGUSR1, sa, NULL) == -1 && ++error_flag)
+		ft_write_u(2, (unsigned char *)"SERVER: SIGUSR1", 15);
+	if (sigaction(SIGUSR2, sa, NULL) == -1 && ++error_flag)
+		ft_write_u(2, (unsigned char *)"SERVER: SIGUSR2", 15);
+	if (sigaction(SIGINT, sa, NULL) == -1 && ++error_flag)
+		ft_write_u(2, (unsigned char *)"SERVER: SIGINT", 14);
+	if (error_flag)
+		exit(1);
+	ft_printf("Server is ready and waiting for signals...\n");
 }
 
 int	main(void)
@@ -41,17 +91,7 @@ int	main(void)
 
 	server_pid = getpid();
 	ft_printf("Server PID: %d\n", server_pid);
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_handler = NULL;
-	sa.sa_sigaction = server_handler;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		return (ft_write_u(2, (unsigned char *)"SERVER: SIGUSR1", 15), 1);
-	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-		return (ft_write_u(2, (unsigned char *)"SERVER: SIGUSR2", 15), 1);
-	if (sigaction(SIGINT, &sa, NULL) == -1)
-		return (ft_write_u(2, (unsigned char *)"SERVER: SIGINT", 14), 1);
-	ft_printf("Server is ready and waiting for signals...\n");
+	for_sigaction(&sa);
 	while (1)
 		pause();
 	return (0);
